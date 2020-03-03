883cb6f4f9413fc9e09706ca3ab1206a52e52e33
okettope <oketonaayoade@gmail.com>
2020-03-03 22:06:40 +0100

    first commit

---
+++ src/fontawesome.js
@@ -0,0 +1,25 @@
+//MY LIBRARY
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+
+//MY ICONS
+import { faMoneyBill } from '@fortawesome/pro-solid-svg-icons';
+import { faCode, faHighlighter } from '@fortawesome/free-regular-svg-icons';
+
+
+library.add(
+    faMoneyBill,
+    faCode,
+    faHighlighter,
+    faUserGraduate,
+    faImages,
+    faGithubAlt,
+    faGoogle,
+    faFacebook,
+    faTwitter,
+    faMoneyBill,
+    faCode,
+    faHighlighter
+    // more icons go here
+  );
--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ import 'semantic-ui-css/semantic.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
+// //FONT AWESOME 
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+
+
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
