d2edb11a3dfad1299d82ebd06dc063e60104505a
Cornelius <oketonaayoade@gmail.com>
2020-03-06 18:21:21 +0100

    Ayoade v 2.9

--- src/components/Portifolio/Pages/AboutPage.js
+++ src/components/Portifolio/Pages/AboutPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import HeroAbout from '../Components/HeroAbout';
 import AboutContent from '../Components/AboutContent';
 import MyProgress from '../Components/MyProgress';
+import { BrowserRouter as Router,Route, Link } from "react-router-dom";
+
 
 
 function AboutPage(props) {
@@ -15,7 +17,7 @@ function AboutPage(props) {
                 I've learnt Graphics Designing for more than Six years and have working experience of not less than Four years. <br/><br/>
                 My dream is to become a competent Full Stack Engineer before the next three years, to start touching the world with my love for innovation, development and technology, making the internet a better place to be.<br/><br/>
                 I am constantly learning new things. Things like Mixed Reality (Aurgumented Reality and Virtual Reality), Artificial Intelligence, Concept of Quantum Computing, Django and React Js. <br/><br/>
-                My latest project, <b>Food n Blog</b>, is a Food/Health Blog/Platform. You can check it out <a rel="noopener noreferrer" target="_blank" href="http://www.google.com">here</a>, or on my <a href="/projects">Project Page</a>. It is being built with React JS and Django.<br/><br/>
+                My latest project, <b>Food n Blog</b>, is a Food/Health Blog/Platform. You can check it out <a rel="noopener noreferrer" target="_blank" href="http://www.google.com">here</a>, or on my <Link to="/projects">Project Page</Link>. It is being built with React JS and Django.<br/><br/>
                 When I am not learning something new, then I am building on what I've known already or creating some Youtube Tutorials for Graphics Designing. You can find those <a  rel="noopener noreferrer" target="_blank" href="https://www.youtube.com/channel/UCM1vsCzdEjSk8KrsOlSPLpQ/videos?view_as=subscriber">here</a>.
                 </p>
             </AboutContent>
