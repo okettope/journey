493595457dccb6e7a2f811879423584b4e1b1b35
cornelius <oketonaayoade@gmail.com>
2020-03-12 23:36:14 +0100

    Ayoade v 3.2 api removed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import React, { Component } from 'react'
 import './App.css'
 
 ///PORTFOLIO IMPORTS///
-import Navbarbar from '../src/components/Portifolio/Components/Navbar';
+// import Navbarbar from '../src/components/Portifolio/Components/Navbar';
 
-
+///LEARNING IMPORTS///
+// import UserGreeting from './components/Learning/UserGreeting'
+import ListRendering from './components/Learning/ListRendering'
 
 ///PORTFOLIO CLASS//////
 class App extends Component {
   render() {
     return (
-      <Navbarbar/>
+      // <Navbarbar/>
+
+      // <UserGreeting/>
+
+      <ListRendering/>
     )
   }
 }
